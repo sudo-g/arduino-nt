5c41b63571d1d4c38a3d5cde04961e760c6dd778
George Xian <george_xian@live.com>
2016-05-31 22:50:43 +1200

    -Allow custom buffer to be specified for NtNode for testability -processBusData() returns bool for error condition, data received is written to pointer.

--- src/nt.cpp
+++ src/nt.cpp
@@ -9,11 +9,11 @@ NtRingBuf::NtRingBuf() : wrtInd(0), unread(0)
 
 }
 
-bool NtRingBuf::pop(uint8_t* data)
+bool NtRingBuf::pop(uint8_t* out)
 {
 	if (unread > 0)
 	{
-		*data = slots[(wrtInd - unread) & BUF_SIZE_MASK];
+		*out = slots[(wrtInd - unread) & BUF_SIZE_MASK];
 		unread--;
 		return true;
 	}
@@ -23,14 +23,20 @@ bool NtRingBuf::pop(uint8_t* data)
 	}
 }
 
+void NtRingBuf::push(uint8_t in)
+{
+	slots[wrtInd++ & BUF_SIZE_MASK] = in;
+	unread++;
+}
+
 // module wide buffer storing incoming serial
 static NtRingBuf ntBuffer = NtRingBuf();
 
 
-NtNode::NtNode(uint8_t id) :
+NtNode::NtNode(uint8_t id, NtRingBuf* buffer) :
 		matchIdGetData(NTBUS_FROM_MASTER | NTBUS_GET | id),
 		busState(IDLE),
-		buffer(&ntBuffer)
+		buffer(buffer)
 {
 	// setting UART to 2MBaud
 	UCSR0A |= (1 << U2X0);
@@ -47,32 +53,42 @@ NtNode::NtNode(uint8_t id) :
 	UCSR0B |= (1<<RXCIE0) | (1<<RXEN0);
 }
 
-uint8_t NtNode::processBusData()
+NtNode NtNode::createNtNode(uint8_t id)
 {
-	uint8_t recv;
-	if (buffer->pop(&recv))
+	NtNode ntNode = NtNode(id, &ntBuffer);
+	return ntNode;
+}
+
+bool NtNode::processBusData(uint8_t* recv)
+{
+	const bool ret = buffer->pop(recv);
+	if (ret)
 	{
 		switch (busState)
 		{
 		case IDLE:
-			if (recv == (NTBUS_STX | NTBUS_TRIGGER))
+			if (*recv == (NTBUS_STX | NTBUS_TRIGGER))
 			{
 				busState = TRIGGERED;
 			}
 			break;
 		case TRIGGERED:
-			if (recv & (1<<7))
+			if (*recv & (1<<7))
 			{
 				busState = GETDATA;
 			}
 			break;
 		case GETDATA:
+			if (*recv == (NTBUS_STX | NTBUS_SET | NTBUS_ID_MOTORALL))
+			{
+				busState = MOTORDATA;
+			}
 			break;
 		case MOTORDATA:
 			break;
 		}
 	}
-	return recv;
+	return ret;
 }
 
 NtNode::NtState NtNode::getBusState() const
@@ -87,25 +103,27 @@ inline void NtNode::write(uint8_t c) const
 }
 
 
-NtNodeImu::NtNodeImu(uint8_t id, tNTBusGetImuData* imudata, uint8_t* imustatus) :
-		NtNode(id),
+NtNodeImu::NtNodeImu(uint8_t id, NtRingBuf* buffer, tNTBusGetImuData* imudata, uint8_t* imustatus) :
+		NtNode(id, buffer),
 		mImudata(imudata),
 		mImustatus(imustatus)
 {
 
 }
 
-void NtNodeImu::processBusData()
+
+bool NtNodeImu::processBusData(uint8_t* recv)
 {
-	uint8_t recv = NtNode::processBusData();
+	bool ret = NtNode::processBusData(recv);
 	if (busState == GETDATA)
 	{
-		if (recv == matchIdGetData)
+		if (*recv == matchIdGetData)
 		{
 			writeImuData();
 		}
 		busState = TRIGGERED;
 	}
+	return ret;
 }
 
 void NtNodeImu::writeImuData() const
