c5617de5a4d1a0104e9d3381a02c4d41b449ff16
George Xian <george_xian@live.com>
2016-06-18 00:29:55 +1200

    Replicated the startup of the original STorM32 as much as possible.

--- arduino_nt.cpp
+++ arduino_nt.cpp
@@ -8,20 +8,25 @@
 int main(void)
 {
 	tNTBusGetImuData imuData;
+	imuData.ImuStatus = NTBUS_IMU_IMUSTATUS_BASE | NTBUS_IMU_IMUSTATUS_GYRODATA_OK | NTBUS_IMU_IMUSTATUS_ACCDATA_OK;
 	memset(&imuData, 0, sizeof(imuData));
 
 	Wire.begin();
 
-
 	MPU6050 imu = MPU6050();
 	imu.initialize();
+	imu.setClockSource(MPU6050_CLOCK_PLL_ZGYRO);
+	imu.setFullScaleGyroRange(MPU6050_GYRO_FS_1000);
+	imu.setExternalFrameSync(MPU6050_EXT_SYNC_DISABLED);
+	imu.setDLPFMode(MPU6050_DLPF_BW_256);
+	imu.setRate(0);    // sample at full speed
 
 	NtNodeImu ntNode = NtNodeImu::getNodeWithNtBuffer(NTBUS_ID_IMU1, "ArduNT IMU", &imuData, NTBUS_IMU_CONFIG_MPU6000);
 
 	for (;;)
 	{
 		imu.getMotion6(&imuData.AccX, &imuData.AccY, &imuData.AccZ, &imuData.GyroX, &imuData.GyroY, &imuData.GyroZ);
-
+		imuData.Temp = imu.getTemperature();
 		uint8_t recv;
 		ntNode.processBusData(&recv);
 	}
